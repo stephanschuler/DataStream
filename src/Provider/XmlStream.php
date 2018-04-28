c0f81372663c30a36d163a0bc664e8dea3b8196f
Stephan Schuler <stephan.schuler@netlogix.de>
2018-04-28 17:14:01 +0200

    Introduce XmlStream provider

---
+++ src/Provider/XmlStream.php
@@ -0,0 +1,51 @@
+<?php
+declare(strict_types=1);
+
+namespace StephanSchuler\DataStream\Provider;
+
+use XMLElementIterator;
+use XMLElementXpathFilter;
+use XMLReader;
+use XMLReaderNode;
+
+class XmlStream implements ProviderInterface
+{
+    use ProviderTrait;
+
+    /**
+     * @var string
+     */
+    protected $fileName;
+
+    /**
+     * @var string
+     */
+    protected $xpath = '/*';
+
+    protected function __construct(string $fileName, string $xpath)
+    {
+        $this->fileName = $fileName;
+        $this->xpath = $xpath;
+    }
+
+    public function provide()
+    {
+        $reader = new XMLReader();
+        $reader->open($this->fileName);
+
+        $iterator = new XMLElementIterator($reader);
+        $list = new XMLElementXpathFilter($iterator, $this->xpath);
+        /** @var XMLReaderNode $element */
+        foreach ($list as $element) {
+            foreach ($this->consumers as $consumer) {
+                $consumer->consume($element->getSimpleXMLElement());
+            }
+        }
+    }
+
+    public static function createProvider(string $fileName, string $xpath = '/*'): XmlStream
+    {
+        $className = get_called_class();
+        return new $className($fileName, $xpath);
+    }
+}
