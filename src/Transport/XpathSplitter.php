e5ad4117052340e85d16e95dd02c6f9b03d09d45
Stephan Schuler <stephan.schuler@netlogix.de>
2018-04-28 17:14:16 +0200

    Introduce XpathSplitter transport

---
+++ src/Transport/XpathSplitter.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+namespace StephanSchuler\DataStream\Transport;
+
+use SimpleXMLElement;
+
+class XpathSplitter implements TransportInterface
+{
+    use SplitterTrait;
+
+    public static function createTransport(string $xpath): XpathSplitter
+    {
+        $splitter = function (SimpleXMLElement $data) use ($xpath) {
+            foreach ($data->xpath($xpath) as $variant) {
+                yield $variant;
+            }
+        };
+        $className = get_called_class();
+        return new $className($splitter);
+    }
+
+}
