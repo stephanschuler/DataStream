e5c1be4e8238aa5471115f368b7703b8978d5e59
Stephan Schuler <stephan.schuler@netlogix.de>
2018-05-05 15:18:03 +0200

    Consume and provide CSV

---
+++ src/Consumer/Csv.php
@@ -0,0 +1,63 @@
+<?php
+declare(strict_types=1);
+
+namespace StephanSchuler\DataStream\Consumer;
+
+use StephanSchuler\DataStream\Runtime\StateBuilder;
+
+class Csv implements ConsumerInterface
+{
+    use ConsumerTrait;
+
+    /**
+     * @var string
+     */
+    protected $fileName;
+
+    /**
+     * @var string
+     */
+    protected $delimiter;
+
+    /**
+     * @var string
+     */
+    protected $enclosure;
+
+    /**
+     * @var string
+     */
+    protected $escape;
+
+    protected $name;
+
+    protected function __construct(
+        string $fileName,
+        string $delimiter = ',',
+        string $enclosure = '"',
+        string $escape = '\\'
+    ) {
+        StateBuilder::getInstance()->addNode($this);
+        $this->fileName = $fileName;
+        $this->delimiter = $delimiter;
+        $this->enclosure = $enclosure;
+        $this->escape = $escape;
+    }
+
+    public static function createConsumer(
+        string $fileName,
+        string $delimiter = ',',
+        string $enclosure = '"',
+        string $escape = '\\'
+    ): Echoing {
+        $className = get_called_class();
+        return new $className($fileName, $delimiter, $enclosure, $escape);
+    }
+
+    public function consume($data)
+    {
+        $fp = fopen($this->fileName, 'a+');
+        fputcsv($fp, $data, $this->delimiter, $this->enclosure, $this->escape);
+        fclose($fp);
+    }
+}
---
+++ src/Source/Csv.php
@@ -0,0 +1,64 @@
+<?php
+declare(strict_types=1);
+
+namespace StephanSchuler\DataStream\Source;
+
+use StephanSchuler\DataStream\Provider\ProviderTrait;
+use StephanSchuler\DataStream\Runtime\StateBuilder;
+use StephanSchuler\DataStream\Scheduler\Scheduler;
+
+class Csv implements SourceInterface
+{
+    use ProviderTrait;
+
+    /**
+     * @var string
+     */
+    protected $fileName;
+
+    /**
+     * @var string
+     */
+    protected $delimiter;
+
+    /**
+     * @var string
+     */
+    protected $enclosure;
+
+    /**
+     * @var string
+     */
+    protected $escape;
+
+    protected function __construct(string $fileName, string $delimiter, string $enclosure, string $escape)
+    {
+        StateBuilder::getInstance()->addNode($this);
+        $this->fileName = $fileName;
+        $this->delimiter = $delimiter;
+        $this->enclosure = $enclosure;
+        $this->escape = $escape;
+    }
+
+    public function provide()
+    {
+        Scheduler::current()->schedule(function () {
+            $fp = fopen($this->fileName, 'r');
+            while ($line = fgetcsv($fp, 0, $this->delimiter, $this->enclosure, $this->escape)) {
+                yield;
+                $this->feedConsumers($line);
+            }
+            fclose($fp);
+        });
+    }
+
+    public static function createSource(
+        string $fileName,
+        string $delimiter = ',',
+        string $enclosure = '"',
+        string $escape = '\\'
+    ): Csv {
+        $className = get_called_class();
+        return new $className($fileName, $delimiter, $enclosure, $escape);
+    }
+}
